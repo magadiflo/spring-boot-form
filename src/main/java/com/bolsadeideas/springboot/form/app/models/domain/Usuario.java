54bd16b566bffe365ca69fafb9783de2959f1e19
Martín <magadiflo@gmail.com>
2022-02-10 18:09:20 -0500

    Creando la clase model del formulario

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -0,0 +1,46 @@
+package com.bolsadeideas.springboot.form.app.models.domain;
+
+public class Usuario {
+
+	private String username;
+	private String password;
+	private String email;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Usuario [username=");
+		builder.append(username);
+		builder.append(", password=");
+		builder.append(password);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
