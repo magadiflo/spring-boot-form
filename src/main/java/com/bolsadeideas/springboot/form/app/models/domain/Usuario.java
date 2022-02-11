318a5606fa3c4526b6dbf448439f0abaae133148
Martín <magadiflo@gmail.com>
2022-02-10 22:26:42 -0500

    La anotación @Size para rangos y @Email para vlaidar correo electrónico

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,24 +1,28 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 public class Usuario {
 
 	private String identificador;
 
-	@NotEmpty //Para las validaciones en el formulario form.html
+	@NotEmpty(message = "El nombre no puede ser vacío¡¡") //Para las validaciones en el formulario form.html
 	private String nombre;
 
 	@NotEmpty
 	private String apellido;
 
 	@NotEmpty
+	@Size(min = 3, max = 8)
 	private String username;
 
 	@NotEmpty
 	private String password;
 
 	@NotEmpty
+	@Email(message = "Correo con formato incorrecto¡¡")
 	private String email;
 
 	public String getUsername() {
