f8f77de2600722c559bfc9d63b2c14ab1dcc208f
Martín <magadiflo@gmail.com>
2022-02-10 22:17:06 -0500

    La anotación @SessionAttributes para mantener los datos durante el ciclo del form

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -8,16 +8,19 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("user")//Permite guardar por sesión el objeto "user". La información que no se use en el formulario no se perderá.
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
-		usuario.setIdentificador("123.456.789-M");
+		usuario.setIdentificador("123.456.789-M");//Este es un atributo que no está en el formulario, pero es parte del objeto. Este atributo no se perdrá
 		usuario.setNombre("Martín");
 		usuario.setApellido("Díaz");
 		
@@ -31,7 +34,7 @@ public class FormController {
 	 * cuando se envie a la vista, esta pueda ser accedido con user
 	 */
 	@PostMapping("/form")
-	public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		
@@ -39,7 +42,11 @@ public class FormController {
 			return "form";
 		}
 				
-		model.addAttribute("usuario", usuario);		
+		model.addAttribute("usuario", usuario);
+		
+		//Elimina el objeto "user" de la sesión (declarado en @SessionAttributes)
+		//Si usamos el @SessionAttributes es importante agregar ese método
+		status.setComplete(); 
 		return "resultado";
 	}
 
