ce575164f5f25f132c4bbfdf0070e0371d94012c
Martín <magadiflo@gmail.com>
2022-02-11 20:02:03 -0500

    Validación de fechas con input type date de HTML5 y @Past y @Future

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -3,11 +3,13 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 import java.util.Date;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -52,6 +54,8 @@ public class Usuario {
 
 	@NotNull
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	//@Past //Valida que sea una fecha pasada a la actual
+	@Future //valida que sea una fecha futura
 	private Date fechaNacimiento;
 
 	public String getUsername() {
